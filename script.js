8cccc29a8db2da1e8f222d31410b98ad6d306469
impozible1 <74678692+impozible1@users.noreply.github.com>
2022-03-24 14:08:32 -0500

    New Update

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function run(){
-	var name = document.getElementById("fileNameInput").value
-	var text = document.getElementById("textarea").value
-	var blob = new Blob([text],
-  { type: "text/plain;charset=utf-8" });
-	saveAs(blob, name);
+function showResult(){
+    let result = document.getElementById("result")
+    result.contentWindow.document.close();
+    result.contentWindow.document.write(document.getElementById("code").value)
 }
+
+function showResultNew(){
+    let windowResult = window.open("about:blank")
+    windowResult.document.close();
+    windowResult.document.write(document.getElementById("code").value)
+}
+
+document.getElementById('code').addEventListener('keydown', function(e) {
+  if (e.key == 'Tab') {
+        e.preventDefault();
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+    
+        this.value = this.value.substring(0, start) +
+        "\t" + this.value.substring(end);
+    
+        this.selectionStart =
+        this.selectionEnd = start + 1;
+    }
+});
